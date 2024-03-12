9eefd5af9e2a917eba5f5cd6320d85608a4693cc
Deepanshu <ideepanshubatra@gmail.com>
2024-03-12 14:28:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <img src="./coding.png" alt="logo" className="logo" id="coding"/>
+        <h2>Sign In to Codes Hub</h2>
+        <h11>Make your devlopment more intresting</h11>
+        <button id="google">
+          <img src="./google.png" alt="logo" className="logo"/>
+            Sign in with Google
+        </button>
+        <button>
+          <img src="./apple.png" alt="logo" className="logo" id="logo-size"/>
+            Sign in with Apple
+        </button>
+        <hr></hr>
+        <span>or</span> 
+        <form>
+          <input type="text" placeholder="phone,email or username"/>
+          <button id="green">Sign in</button>
+        </form>
+        <button id="filled">Forgot Password</button>
+        <p>Don't have an account? <a>Sign up</a></p>
+      </div> 
     </div>
   );
 }
